871f85298486076f0f82ff6cdcd710aae5302731
DevKDuck <loading95@naver.com>
2024-10-29 11:47:41 +0900

    Initial commit

---
+++ src/main/java/com/devkduck/letterfromthesea/LetterfromtheseaApplication.java
@@ -0,0 +1,13 @@
+package com.devkduck.letterfromthesea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterfromtheseaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterfromtheseaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devkduck/letterfromthesea/LetterfromtheseaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkduck.letterfromthesea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterfromtheseaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
