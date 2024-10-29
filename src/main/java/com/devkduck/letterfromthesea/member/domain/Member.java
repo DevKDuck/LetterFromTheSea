56e716c223a3b66a7a3cb0f31c044268a182912c
DevKDuck <loading95@naver.com>
2024-10-29 12:31:29 +0900

    [feat:] sercurity test

---
+++ src/main/java/com/devkduck/letterfromthesea/member/domain/Member.java
@@ -0,0 +1,8 @@
+package com.devkduck.letterfromthesea.member.domain;
+
+public class Member {
+    private Long id;
+    private String email;
+    private String name;
+    private String password;
+}
